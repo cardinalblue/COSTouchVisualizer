e5a199209aeeac5fd220bab421c0a7a9bcbe56dd
yyjim <yy.jim731@gmail.com>
2025-07-29 19:51:11 +0800

    feat: Swift Package Manager support

---
+++ Sources/COSTouchVisualizer/include/COSTouchVisualizer.h
@@ -0,0 +1,18 @@
+//
+//  COSTouchVisualizer.h
+//  COSTouchVisualizer
+//
+//  Created by Jim Wang on 2025/7/29.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TouchVisualizer.
+FOUNDATION_EXPORT double COSTouchVisualizerVersionNumber;
+
+//! Project version string for TouchVisualizer.
+FOUNDATION_EXPORT const unsigned char COSTouchVisualizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <COSTouchVisualizer/PublicHeader.h>
+
+#import "COSTouchVisualizerWindow.h"
